f3d5b4ffccb4235888df5fbb393152474bd40073
hmauroy <hmauroy@gmail.com>
2025-12-16 21:40:45 +0100

    boblespill v2: startet å implementere Fiende-klassen. Skal bevege seg tilfeldig i 3 sekunder av gangen.

--- fiende.py
+++ fiende.py
@@ -1,14 +1,25 @@
 """ Klassedefinisjon av Fiende"""
 from boble import Boble
+from random import random,randrange
 
 class Fiende(Boble):
+    """
+    Klassen Fiende.
+    Driver rundt med tilfeldig fart i 
+    en retning i 2-3 sekunder. Så endres farten.
+    """
     def __init__(self, r, x, y):
         super().__init__(r, x, y)
         self.type = "fiende"
+        self.frameTeller = 0
     
     def oppdater(self):
         """
         Delvis overstyring av superklassens oppdater().
         Skal generere tilfeldig fart og bevegelse.
         """
-        pass
+        self.frameTeller += 1
+        if self.frameTeller % 90 == 0:
+            self.frameTeller = 0
+            self.dx = random() * randrange(True,False)
+            self.dy = random() * randrange(True,False)
